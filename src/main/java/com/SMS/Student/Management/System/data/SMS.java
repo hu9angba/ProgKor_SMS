a088eb02e55ac2f93908be70690cf2150372dcc3
hu9angba <angelika99balogh@gmail.com>
2023-05-31 18:53:08 +0200

    #2 modify pom.xml

--- src/main/java/com/SMS/Student/Management/System/data/SMS.java
+++ src/main/java/com/SMS/Student/Management/System/data/SMS.java
@@ -95,7 +95,7 @@ public class SMS {
 
         @Override
         public String toString() {
-            return "Song{"
+            return "sms{"
                     + "id=" + id
                     + ", nev='" + name + '\''
                     + ", tantargy='" + subject + '\''
