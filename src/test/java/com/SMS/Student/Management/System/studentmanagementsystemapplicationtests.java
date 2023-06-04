86da06bc9d9a0630fbf34e3c658c5ffc1e764e27
hu9angba <angelika99balogh@gmail.com>
2023-06-04 22:17:30 +0200

    #8

--- src/test/java/com/SMS/Student/Management/System/studentmanagementsystemapplicationtests.java
+++ src/test/java/com/SMS/Student/Management/System/studentmanagementsystemapplicationtests.java
@@ -1,4 +1,4 @@
-package com.SMS.Student.Management.System;
+package com.sms.student.management.system;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
